3dc5e448a6c371d2f2b66c6f31008cfdc01e88bb
de140 <phucnnlde140045@fpt.edu.vn>
2022-05-30 17:21:08 +0700

    nop bai

--- bai4_databinding/thuc_hanh/ung_dung_login/src/main/java/com/codegym/model/User.java
+++
@@ -1,9 +0,0 @@
-package com.codegym.model;
-
-public class User {
-    private String account;
-    private String password;
-    private String name;
-    private String email;
-    private int Age;
-}
